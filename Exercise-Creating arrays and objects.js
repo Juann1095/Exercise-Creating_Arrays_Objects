7a7b212a8797bfb695ada4438d2a3e2b0627877b
Juan Romero <slayerforce880@gmail.com>
2023-03-29 15:43:35 -0600

    Exercise Upload
    Exercise creating arrays and objects.

---
+++ Exercise-Creating arrays and objects.js
@@ -0,0 +1,13 @@
+var clothes = [];
+//Add 5 favorite items of clothing as strings using push() method
+clothes.push("Jeans", "T-shirt","shorts","Shoes","Boots" );
+console.log(clothes);
+//Remove the fifth piece of clothing from the array using pop() method
+clothes.pop(4);
+console.log(clothes);
+//Add new piece of clothing using the push() method
+clothes.push("Socks")
+//Use console.log to show the third item from the clothes array in the console
+console.log(clothes[4])
+//Create a new empty object literal and assign it to the variable favCar
+var favCar = {}
